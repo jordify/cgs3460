555a3af5ccb020cfb66c0117f9fab430e0d60427
Jorge Gomez <jordify221@gmail.com>
2011-07-29 11:33:04 -0400

    Starting HW6

---
+++ hw6/jpeg.h
@@ -0,0 +1,4 @@
+// this function reads a JPEG image from file named input_file_name
+// and stores the pixel data in the file named output_file_name
+// The function returns the number of bytes written
+int loadJPG(const char* input_file_name, const char * output_file_name);
